9fbfd417150341a7aca0bbec61243eddd48d69ff
Gmm <gusenmagomedov13@gmail.com>
2022-06-23 13:49:56 +0300

    Add files via upload

---
+++ lesson.js
@@ -0,0 +1,54 @@
+let item_1 
+item_1 = 5;
+console.log(item_1);
+let item_2
+item_2 = 3;
+console.log(item_2);
+let item_3;
+console.log(item_3)
+item_3 = item_1 + item_2;
+let item_4
+tem_4 = "yolochka";
+console.log("yolochka");
+
+console.log(item_3 = item_2 + item_1);
+console.log(item_3 * item_4);
+//console.log(typeof "yolochka");
+let item_5 = item_3;
+item_5 = item_3;
+let item_6
+item_6 = 15;
+let item_6_type;
+item_6_type = typeof(item_6);
+console.log('item_6 =='  + item_6 + ',' + 'item_6_type ==' + item_6_type);
+let item_7 = string(item_6);
+let item_7_type;
+item_7_type = typeof(item_7);
+console.log('item_7 ==' + item_7 + ',' + 'item_7_type ==' + item_7_type);
+
+let age_1 = 10;
+let age_2 = 18;
+let age_3 = 60;
+
+let compare_1 = age_1 < age_2;
+let compare_2 = age_1 >= age_2;
+let compare_3 = age_1 < age_3;  
+let compare_4 = age_1 > age_3;
+
+//let condition = true;
+
+ if (compare_1) {
+    //let compare_1 = age_1 < age_2;
+    console.log("you do not have access csuse your age is "+ age_1 +" it is less then")
+
+}else if (compare_2) {
+    //let compare_2 = age_1 >= age_2;
+    console.log("Welcome!") 
+}
+ if (compare_3) {
+    //let compare_3 = age_1 < age_3; 
+    console.log("tehnical work")
+} else {
+    //let compare_4 = age_1 > age_3;
+    console.log("Keep calm and look Culture channel")
+}  
